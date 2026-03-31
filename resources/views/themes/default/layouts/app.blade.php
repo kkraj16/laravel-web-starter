507eeb213fea0038a29460b17d846a8e2ccc2c22
kamal Rajpurohit <kkrajpurohit007@gmail.com>
2026-03-31 13:16:55 +0530

    feat: update site favicon assets and integrate multi-size icon links in layout template

--- resources/views/themes/default/layouts/app.blade.php
+++ resources/views/themes/default/layouts/app.blade.php
@@ -4,6 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $seo->meta_title ?? config('app.name') }}</title>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('uploads/system/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('uploads/system/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('uploads/system/apple-touch-icon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     
     @if(!request()->routeIs('products.show'))
     <!-- Dynamic SEO Tags -->
